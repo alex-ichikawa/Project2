0002c35dad3fcbed43a550cee81914e7d13fa353
Alex Ichikawa <aichikawa@isolitesystems.com>
2018-11-24 14:06:09 -0800

    user name displays on home page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,53 +2,55 @@ require("dotenv").config();
 let request = require("request");
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
+  app.get("/", function (req, res) {
     // db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        // examples: dbExamples
-      });
+    res.render("index", {
+      msg: "Welcome!",
+      // examples: dbExamples
     });
-
-  app.get("/signin", function(req, res){
-      res.render("signin");
   });
 
-  app.get("/signup", function(req, res){
-      res.render("signup");
+  app.get("/signin", function (req, res) {
+    res.render("signin");
+  });
+
+  app.get("/signup", function (req, res) {
+    res.render("signup");
   });
   // });
 
-  app.get("/home/:id", function(req, res) {
-    console.log(`user logged in ${req.firstName} ${req.lastName}`);
-    res.render("home", {
-      
+  app.get("/home/:id/:firstName", function (req, res) {
+    let id = req.params.id;
+    db.User.findOne({ where: { id: id, } }).then(function (user) {
+      res.render("home", {
+        user: user
+      });
     });
   });
 
-  app.get("/location/:name/:address", function(req, res){
-    let name = req.params.name;
-    let address = req.params.address;
-    // let longitude = req.params.longitude;
-    request(`https://data.cityofchicago.org/resource/cwig-ma7x.json?dba_name=${name}&address=${address}%20&$order=inspection_date DESC&$$app_token=${process.env.chicagoAPI}`, function (err, response, body) {
-      if (!err && response.statusCode === 200) {
-        locationInfo = JSON.parse(body);
-        console.log(locationInfo);
-        res.render("location", {
-          locations: locationInfo
-        });
-      }
-      else {
-        console.log(err);
-      }
+  app.get("/location/:name/:address", function (req, res) {
+      let name = req.params.name;
+      let address = req.params.address;
+      // let longitude = req.params.longitude;
+      request(`https://data.cityofchicago.org/resource/cwig-ma7x.json?dba_name=${name}&address=${address}%20&$order=inspection_date DESC&$$app_token=${process.env.chicagoAPI}`, function (err, response, body) {
+        if (!err && response.statusCode === 200) {
+          locationInfo = JSON.parse(body);
+          console.log(locationInfo);
+          res.render("location", {
+            locations: locationInfo
+          });
+        }
+        else {
+          console.log(err);
+        }
+      });
     });
-  });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.get("/example/:id", function (req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
       res.render("example", {
         example: dbExample
       });
@@ -56,7 +58,7 @@ module.exports = function(app) {
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
