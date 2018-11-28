2c54e5b15ba3b6f5490528f63171e0701e1a80f2
Alex Ichikawa <aichikawa@isolitesystems.com>
2018-11-27 23:08:38 -0800

    added else if for process.env.node_env in server.js

--- server.js
+++ server.js
@@ -41,6 +41,9 @@ var syncOptions = { force: false };
 if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
+else if (process.env.NODE_ENV === 'production'){
+	app.use(express.static('client/build'));
+}
 
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function() {
